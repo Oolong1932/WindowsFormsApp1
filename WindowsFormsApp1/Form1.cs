cca5a01ea804c8c0a391f3e9b11940ac203173bc
珈榕 李 <rong1932@hotmail.com>
2019-05-20 11:48:14 +0800

    Form1_button 更改_0520

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            label1.Text = "11111";
+            label1.Text = "2222";
         }
     }
 }
